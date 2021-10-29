8f305f4d8271d3be07948742d55d11aec1712c95
Gokul Sudheesh <gokulsudheesh1@gmail.com>
2021-10-29 10:40:13 +0400

    keyword search

--- app.js
+++ app.js
@@ -14,14 +14,39 @@ const PREDICT_HQ_TOKEN = process.env.PREDICT_HQ_TOKEN;
 const client = new phq.Client({access_token: PREDICT_HQ_TOKEN, fetch: crossFetch});
 
 app.get("/", function(req, res){
-    client.events.search({ country: "AE", limit: 100 })
+    const today  = new Date().toISOString().slice(0,10);
+    client.events.search({ country: "AE", limit: 500, "start.gte": today })
     .then(
         (results) => {
             // console.log(results.result.results);
             res.render("index", { eventsJSON: JSON.stringify({ events: results.result.results }).replace(/\\/g, '\\\\').replace(/"/g, '\\\"') });
         }
     ).catch(
-        err => console.error(err)
+        (err) => {
+            console.error(err);
+            res.sendFile(__dirname + "/failure.html");
+        }    
+    );
+});
+
+app.get("/category/:category", function(req, res){
+    console.log(req.params);
+    res.redirect("/");
+});
+
+app.get("/search/:q", function(req, res){
+    const today  = new Date().toISOString().slice(0,10);
+    client.events.search({ country: "AE", limit: 500, "start.gte": today, ...req.params })
+    .then(
+        (results) => {
+            // console.log(results.result.results);
+            res.render("index", { eventsJSON: JSON.stringify({ events: results.result.results }).replace(/\\/g, '\\\\').replace(/"/g, '\\\"') });
+        }
+    ).catch(
+        (err) => {
+            console.error(err);
+            res.sendFile(__dirname + "/failure.html");
+        }    
     );
 });
 
